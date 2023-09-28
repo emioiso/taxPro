b1d0b7aa58bc4c685d6015aab866a91a908da378
emioiso <jijjjiijiijijiji@gmail.com>
2023-09-28 22:06:51 +0900

    Initial Commit

---
+++ taxPro/taxProApp.swift
@@ -0,0 +1,17 @@
+//
+//  taxProApp.swift
+//  taxPro
+//
+//  Created by emi oiso on 2023/09/28.
+//
+
+import SwiftUI
+
+@main
+struct taxProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
